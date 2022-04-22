e55dd7b385c30864e4a2f58d04bf6cf829f28fc7
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-04-22 12:14:54 -0700

    commit

--- client/src/components/PersonCard.js
+++ client/src/components/PersonCard.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      addYear:this.props.age,
+    };
+  }
 
 render() {
-const { firstName, lastName, age,  hairColor } = this.props;
+const { firstName, lastName, hairColor } = this.props;
   return (
     <div>
       <h2> {lastName}, {firstName}</h2>
-      <p>Age: {age} </p>
+      <p>Age: {this.state.addYear} </p>
       <p>Hair Color: {hairColor} </p>
+      <button className="birthday" onClick={() => this.setState({ addYear: this.state.addYear + 1})}>  Happy Birthday </button>
     </div>
   );
  }
